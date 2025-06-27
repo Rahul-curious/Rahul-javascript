8d45a83dc121ebbb22122f2b76382154ebfd38fb
Rahul-curious <22bcs15602@cuchd.in>
2025-06-27 12:16:46 +0000

    variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,15 @@
+const account_Id = 14433;
+let account_Email = "rahul2gmail.com";
+var account_Password = "12345";
+accountcity = "jaipur";
+ 
+// console.log(account_Id); or id cant be change 
+// var is not prefer to use 
+// let will use in next whenever we declare variables 
+//  only variables should be change not id    
+ account_Email = "efnf@gmail.com"
+ account_Password ="22222"
+ console.log(account_Id)
+ console.log (accountstate)
+
+ console.table([account_Email,account_Id,account_Password ,accountcity])
